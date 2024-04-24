7a1a62a4abf36f4bd15bfa7315466b745ddf7ee2
mahekkg <mahekg2001@gmail.com>
2024-04-24 23:40:27 +0530

    ui improvements

--- src/Typewriter.jsx
+++ src/Typewriter.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import typewriterSound from './assets/sound.wav'
+import React, { useState, useEffect } from "react";
+import typewriterSound from "./assets/sound.wav";
 
 const TypewriterApp = () => {
   const [typewriterEnabled, setTypewriterEnabled] = useState(false);
@@ -9,16 +9,16 @@ const TypewriterApp = () => {
       if (typewriterEnabled) {
         // Check if a key is pressed here
         console.log("Key pressed:", event.key);
-        
+
         // Play typewriter sound
         new Audio(typewriterSound).play();
       }
     };
 
-    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener("keydown", handleKeyDown);
 
     return () => {
-      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener("keydown", handleKeyDown);
     };
   }, [typewriterEnabled]);
 
@@ -27,11 +27,17 @@ const TypewriterApp = () => {
   };
 
   return (
-    <div>
-      <button onClick={toggleTypewriterSound}>
-        {typewriterEnabled ? 'Disable' : 'Enable'} Typewriter Sound
+    <div class="flex flex-col items-center justify-center w-40 h-20">
+      <button
+        onClick={toggleTypewriterSound}
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        {typewriterEnabled ? "Disable" : "Enable"} Typewriter Sound
       </button>
-      <textarea />
+      <textarea
+        class="mt-4 border rounded-md px-4 py-2"
+        placeholder="Type here..."
+      ></textarea>
     </div>
   );
 };
